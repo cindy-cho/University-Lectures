f51bd39fd0731738c83818c6bfd8a70aaea54ef5
yoohwa-cho <43ryuflower@gmail.com>
2021-06-18 11:12:42 +0900

    python turtle practice - draw house

---
+++ 컴퓨팅 사고력/final 대비/turtle practice.py
@@ -0,0 +1,72 @@
+from turtle import*
+
+
+##pencolor("red")
+##fd(100)
+##rt(90)
+##fd(100)
+##rt(90)
+##fd(100)
+##rt(90)
+##fd(100)
+##rt(90)
+
+
+##pencolor("blue")
+##for count in range(6):
+##    circle(100)
+##    left(360/6)
+##
+##
+##pencolor("black")
+##shape("turtle")
+##
+##for i in range(3):
+##    forward(100)
+##    left(360/3)
+##
+##pencolor("purple")
+##penup()
+##goto(200,0)
+##pendown()
+##
+##for i in range(4):
+##    forward(100)
+##    left(360/4)
+##
+##penup()
+##
+##goto(-200,0)
+##pendown()
+##width(20)
+##color("BLUE")
+##shape("turtle")
+##begin_fill()
+##fillcolor("RED")
+##fd(200)
+##lt(120)
+##fd(200)
+##lt(120)
+##fd(200)
+##end_fill()
+
+
+width(3)
+pencolor("brown")
+shape("turtle")
+
+begin_fill()
+fillcolor("yellow")
+for i in range(3):
+    fd(200)
+    left(360/3)
+end_fill()
+begin_fill()
+fillcolor("green")
+right(90)
+for i in range(4):
+    fd(200)
+    left(360/4)
+
+end_fill()
+
